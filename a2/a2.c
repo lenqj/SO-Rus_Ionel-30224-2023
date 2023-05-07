68a8d82303bf97f5c749e5e01024ffbd0f4222b9
lenqj <ionelrus022@yahoo.com>
2023-05-07 19:23:28 +0300

    testeee

--- a2/a2.c
+++ a2/a2.c
@@ -4,22 +4,47 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include "a2_helper.h"
+typedef struct THS
+{
+    int value;
+    sem_t *logSem[2];
+} THS;
 
 void *thf(void *arg)
 {
-    int s = *(int *)arg;
-    info(BEGIN, 7, s);
-    info(END, 7, s);
-    //exit(0);
+    THS *s = (THS*)arg;
+    if (s->value == 2)
+    {
+        info(BEGIN, 7, s->value);
+        sem_post(s->logSem[0]);
+        sem_wait(s->logSem[1]);
+        info(END, 7, s->value);
+    }
+    else if (s->value == 4)
+    {
+        sem_wait(s->logSem[0]);
+        info(BEGIN, 7, s->value);
+        info(END, 7, s->value);
+        sem_post(s->logSem[1]);
+    }
+    else
+    {
+        info(BEGIN, 7, s->value);
+        info(END, 7, s->value);
+    }
+    // exit(0);
     return NULL;
 }
 void *thf2(void *arg)
 {
-    int s = *(int *)arg;
-    info(BEGIN, 6, s);
-    info(END, 6, s);
-    //exit(0);
+    THS *s = (THS*)arg;
+    sem_wait(s->logSem[0]);
+    info(BEGIN, 6, s->value);
+    info(END, 6, s->value);
+    sem_post(s->logSem[0]);
+    // exit(0);
     return NULL;
 }
 void *thf3(void *arg)
@@ -27,7 +52,7 @@ void *thf3(void *arg)
     int s = *(int *)arg;
     info(BEGIN, 3, s);
     info(END, 3, s);
-    //exit(0);
+    // exit(0);
     return NULL;
 }
 
@@ -44,16 +69,16 @@ int main()
             info(BEGIN, 3, 0);
 
             pthread_t threads[6];
-                int v[6];
-                for (int i = 0; i < 6; i++)
-                {
-                    v[i] = i+1;
-                    pthread_create(&threads[i], NULL, thf3, &v[i]);
-                }   
-                for (int i = 0; i < 6; i++)
-                {
-                    pthread_join(threads[i], NULL);
-                }
+            int v[6];
+            for (int i = 0; i < 6; i++)
+            {
+                v[i] = i + 1;
+                pthread_create(&threads[i], NULL, thf3, &v[i]);
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                pthread_join(threads[i], NULL);
+            }
             if (fork() == 0)
             {
                 info(BEGIN, 4, 0);
@@ -69,12 +94,15 @@ int main()
             {
                 info(BEGIN, 6, 0);
                 pthread_t threads[37];
-                int v[37];
+                THS params[37];
+                sem_t logSem;
+                sem_init(&logSem, 0, 6);
                 for (int i = 0; i < 37; i++)
                 {
-                    v[i] = i+1;
-                    pthread_create(&threads[i], NULL, thf2, &v[i]);
-                }   
+                    params[i].value = i + 1;
+                    params[i].logSem[0] = &logSem;
+                    pthread_create(&threads[i], NULL, thf2, &params[i]);
+                }
                 for (int i = 0; i < 37; i++)
                 {
                     pthread_join(threads[i], NULL);
@@ -97,17 +125,25 @@ int main()
         }
         if (fork() == 0)
         {
-            pthread_t threads[4];
             info(BEGIN, 7, 0);
-            int v[] = {1, 2, 3, 4};
+            pthread_t threads[4];
+            THS params[4];
+            sem_t logSem[2];
+            sem_init(&logSem[0], 0, 1);
+            sem_init(&logSem[1], 0, 1);
             for (int i = 0; i < 4; i++)
             {
-                pthread_create(&threads[i], NULL, thf, &v[i]);
+                params[i].value = i + 1;
+                params[i].logSem[0] = &logSem[0];
+                params[i].logSem[1] = &logSem[1];
+                pthread_create(&threads[i], NULL, thf, &params[i]);
             }
             for (int i = 0; i < 4; i++)
             {
                 pthread_join(threads[i], NULL);
             }
+            sem_destroy(&logSem[0]);
+            sem_destroy(&logSem[1]);
             info(END, 7, 0);
             exit(0);
         }
